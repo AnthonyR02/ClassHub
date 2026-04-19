2b2d2c308af0872ac6296d3cf5752fd136c3e160
Vince Mikel Masong <vincemikelmasong724@gmail.com>
2026-04-18 22:38:13 -0400

    trying to make a screen where the user is supposed to go to the username and password field when clicking login, needs a separate fxml file for now to work

--- src/main/java/org/example/loginscreen/HelloController.java
+++ src/main/java/org/example/loginscreen/HelloController.java
@@ -1,14 +1,31 @@
 package org.example.loginscreen;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+
+    //when clicking on the login button, is supposed to take the user to the login screen (user and pass)
+    // as for now, it does not work. I still have to find the issue and make a loginview fxml file
+    public void goToLogin(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("login-view.fxml"));
+            Parent root = loader.load();
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
--- src/main/java/org/example/loginscreen/Launcher.java
+++ src/main/java/org/example/loginscreen/Launcher.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 
 public class Launcher {
     public static void main(String[] args) {
+
         Application.launch(HelloApplication.class, args);
     }
 }
@@ -12,3 +13,4 @@ public class Launcher {
 4/17/2026 [Vince Masong]
 I haven't added any content yet, but I just want to get javafx set up for now since I'm
 having a huge issue with the configuration and version compatibility. Will continue this tomorrow */
+
